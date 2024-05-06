d6c03c2a995899aaada844f2e672a2f7fe3260ed
Mccranky83 <ericcai20021118@gmail.com>
2024-05-06 13:15:54 +0800

    Resolve nested import of cmp sources

--- lua/mccranky/plugins/autocomplete.lua
+++ lua/mccranky/plugins/autocomplete.lua
@@ -2,7 +2,7 @@ return {
   "zbirenbaum/copilot-cmp",
   after = { "copilot.lua", "nvim-cmp" }, -- race condition
   dependencies = {
-    { "uga-rosa/cmp-dictionary", lazy = true }, -- not necessarily lazy
+    { "uga-rosa/cmp-dictionary", lazy = true },
   },
   config = function()
     local cmp = require("cmp")
@@ -23,63 +23,65 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
     end
 
+    require("cmp_dictionary").setup({
+      paths = { "/usr/share/dict/words" },
+      exact_length = 2,
+      first_case_insensitive = true,
+    })
+
     require("copilot_cmp").setup({
-      require("cmp_dictionary").setup({
-        paths = { "/usr/share/dict/words" },
-        exact_length = 2,
-        first_case_insensitive = true,
-      }),
-      cmp.setup({
-        sources = {
-          { name = "nvim_lsp", group_index = 2 },
-          { name = "path", group_index = 2 },
-          { name = "buffer", group_index = 2 },
-          { name = "luasnip", group_index = 2 },
-          { name = "copilot", max_item_count = 3, group_index = 2 },
-          {
-            name = "dictionary",
-            max_item_count = 10,
-            keyword_length = 2,
-            group_index = 2,
-          },
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            -- mode = "symbol", -- no text
-            max_width = 50,
-            symbol_map = { Copilot = "" },
-            ellipsis_char = "...",
-          }),
-        },
-        mapping = {
-          ["<Tab>"] = vim.schedule_wrap(function(fallback)
-            if cmp.visible() and has_words_before() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end),
-        },
-        sorting = {
-          priority_weight = 2,
-          comparators = {
-            require("copilot_cmp.comparators").prioritize,
-            -- Below is the default comparitor list and order for nvim-cmp
-            cmp.config.compare.offset,
-            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.locality,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
-      }),
       event = { "InsertEnter", "LspAttach" },
       fix_pairs = true,
+    })
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "copilot", max_item_count = 3, group_index = 2 },
+        {
+          name = "dictionary",
+          max_item_count = 10,
+          keyword_length = 2,
+          group_index = 2,
+        },
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          -- mode = "symbol", -- no text
+          max_width = 50,
+          symbol_map = { Copilot = "" },
+          ellipsis_char = "...",
+        }),
+      },
+      mapping = {
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
     })
   end,
 }
