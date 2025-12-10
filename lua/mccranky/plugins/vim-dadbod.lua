a3d37ef38006f2f40e4d4de03b83d79d74bb7cfd
Mccranky83 <berkeleyblue02@gmail.com>
2025-12-10 12:05:23 -0600

    Remove vim-dotenv dependency from vim-dadbod config

--- lua/mccranky/plugins/vim-dadbod.lua
+++ lua/mccranky/plugins/vim-dadbod.lua
@@ -3,27 +3,29 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-    dependencies = { "tpope/vim-dadbod", "tpope/vim-dotenv" },
+    dependencies = { "tpope/vim-dadbod" },
     config = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "DBUIReady",
-        callback = function()
-          local path = "~/.env"
-          vim.cmd("Dotenv " .. path)
-        end,
-      })
+      vim.g.dbs = {
+        {
+          name = "LocalMySQL",
+          url = "mysql://root@localhost:3306/",
+        },
+        {
+          name = "LocalMongo",
+          url = "mongodb://localhost:27017/",
+        },
+      }
+      vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
   {
     "kristijanhusak/vim-dadbod-completion",
     ft = { "sql", "mysql", "plsql", "psql" },
-    dependencies = { "tpope/vim-dadbod", "tpope/vim-dotenv" },
+    dependencies = { "tpope/vim-dadbod" },
     config = function()
       vim.api.nvim_create_autocmd("FileType", {
         pattern = { "sql", "mysql", "plsql", "psql" },
         callback = function()
-          local path = "~/.env"
-          vim.cmd("Dotenv " .. path)
           require("cmp").setup.buffer({
             sources = {
               { name = "vim-dadbod-completion" },
